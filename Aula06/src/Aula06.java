86b7750e4de69be69da1fe1aed24fb83f842cd4e
Thiago Almeida <thiago.ralmeida@outlook.com>
2024-04-01 11:40:54 -0300

    Encapsulamento

---
+++ Aula06/src/Aula06.java
@@ -0,0 +1,13 @@
+public class Aula06 {
+    public static void main(String[] args) {
+        ControleRemoto remote = new ControleRemoto();
+        remote.ligar();
+        remote.play();
+        remote.maisVolume();
+        remote.maisVolume();
+        remote.maisVolume();
+        remote.maisVolume();
+        remote.abrirMenu();
+
+    }
+}
---
+++ Aula06/src/Controlador.java
@@ -0,0 +1,12 @@
+public interface Controlador {
+    public abstract void ligar();
+    public abstract void desligar();
+    public abstract void abrirMenu();
+    public abstract void fecharMenu();
+    public abstract void maisVolume();
+    public abstract void menosVolume();
+    public abstract void muteOn();
+    public abstract void muteOff();
+    public abstract void play();
+    public abstract void pause();
+}
