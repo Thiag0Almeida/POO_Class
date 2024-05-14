1111953435b2b9ad0eb7e30a962fc68a238c52ed
Thiago Almeida <thiago.ralmeida@outlook.com>
2024-05-14 11:23:19 -0300

    Aula Extra - For

---
+++ AulaExtra-For/src/Contagem/Contagem.java
@@ -0,0 +1,16 @@
+package Contagem;
+public class Contagem {
+    public static void main(String[] args) {
+        System.out.println("Contagem:");
+        for(int i = 0; i < 5; i++){
+            System.out.println(i);
+        }
+
+        System.out.println("----------------------");
+
+        System.out.println("Regressiva:");
+        for(int i = 4; i >=0; i--){
+            System.out.println(i);
+        }
+    }
+}
